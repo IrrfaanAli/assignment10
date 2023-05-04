3557b7febeecbd9e623eb7ed02cc3fa9ee81c781
Irfan Ali <alimohammadirfan7@gmail.com>
2023-05-04 23:11:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port =  process.env.PORT || 5000;
+
+const chefsData = require('./store/chefs.json');
+
+app.use(cors());
+
+app.get('/',(req,res)=>{
+   res.send(chefsData);
+})
+
+app.get('/chef',(req,res)=>{
+   res.send(chefsData);
+})
+
+app.get('/chef/:id',(req,res)=>{
+
+    const id = req.params.id;
+    
+    const selectedChef = chefsData.find(data => data.chef_id === parseInt(id) );
+
+    res.send(selectedChef);
+})
+
+
+app.listen(port,()=>{
+    console.log("App running");
+})
